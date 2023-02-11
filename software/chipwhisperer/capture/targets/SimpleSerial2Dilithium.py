c2cb8607e14e161a18ac62b443b797029e888b24
Your Name <you@example.com>
2023-02-11 17:47:07 +0100

    lot of changed accumulated ...

--- software/chipwhisperer/capture/targets/SimpleSerial2Dilithium.py
+++ software/chipwhisperer/capture/targets/SimpleSerial2Dilithium.py
@@ -1,3 +1,5 @@
+import functools
+
 import chipwhisperer.capture.targets.SimpleSerial2 as SimpleSerial2
 import struct
 import importlib
@@ -19,8 +21,10 @@ class TargetIOError(BlockingIOError):
         self.__data = data
 
 class TargetTimeoutError(TargetIOError):
-    def __init__(self):
-        super().__init__('Target cleanly timed out while generating a signature', b'')
+    def __init__(self, message: str = None):
+        if message is None:
+            message = 'Target cleanly timed out while generating a signature'
+        super().__init__(message, b'')
 
 
 class LogToExceptionHandler(logging.NullHandler):
@@ -53,7 +57,7 @@ class SimpleSerial2Dilithium(SimpleSerial2):
     __handler = LogToExceptionHandler()
     __ALGORITHMS = [2, 3, 5]
     __MAX_PAYLOAD_LENGTH = 128 #64 #120 #128 #249
-    
+
     __COMMAND_ALGORITHM = 'q'
     __COMMAND_SET_SECRET_KEY = 'k'
     __COMMAND_SIGN = 'e'
@@ -66,11 +70,21 @@ class SimpleSerial2Dilithium(SimpleSerial2):
     # 100 Dilithium (2) signature timings in seconds of messages (0x0000 - 0x0064)
     __SIG_TIMINGS = [1.8345985412597656, 0.571202278137207, 0.5727174282073975, 2.44435453414917, 1.9954373836517334, 1.956618309020996, 1.0284478664398193, 0.5720925331115723, 1.0222575664520264, 0.5756478309631348, 3.327014446258545, 1.1550006866455078, 3.5656371116638184, 1.4125359058380127, 1.2258155345916748, 0.5754551887512207, 0.5706188678741455, 2.6741480827331543, 1.2207884788513184, 0.569699764251709, 1.8295707702636719, 2.4150185585021973, 2.5315682888031006, 1.6380705833435059, 0.5689265727996826, 0.7610783576965332, 0.800501823425293, 2.897522211074829, 1.539381504058838, 1.148653507232666, 0.5714447498321533, 2.256739377975464, 0.795245885848999, 0.956822395324707, 0.5665726661682129, 0.5721073150634766, 1.150632381439209, 1.2155184745788574, 1.6022050380706787, 0.9530470371246338, 0.7947635650634766, 1.220771312713623, 1.7636890411376953, 0.7999839782714844, 2.0266835689544678, 2.9747705459594727, 1.5745739936828613, 0.5741910934448242, 1.4473426342010498, 1.153580665588379, 0.5740790367126465, 0.7645726203918457, 0.5718286037445068, 3.671562433242798, 1.5743498802185059, 2.4453606605529785, 0.5700263977050781, 0.7969129085540771, 0.5690975189208984, 1.2237257957458496, 0.571495532989502, 0.9935479164123535, 0.7666583061218262, 1.0304381847381592, 0.8014285564422607, 0.5709078311920166, 0.7657623291015625, 0.7964329719543457, 1.832737922668457, 0.5714242458343506, 1.1860861778259277, 0.571995735168457, 0.7650182247161865, 1.0285441875457764, 2.029315710067749, 0.987626314163208, 0.7646205425262451, 2.2466177940368652, 1.6047735214233398, 0.7635841369628906, 0.5692150592803955, 0.7618019580841064, 1.6753244400024414, 0.9920835494995117, 0.5694153308868408, 0.7975795269012451, 1.4073009490966797, 2.9902737140655518, 0.5741786956787109, 0.8005082607269287, 2.445647716522217, 0.5705244541168213, 0.993781566619873, 0.7978324890136719, 3.0634548664093018, 0.7647135257720947, 0.7613015174865723, 1.8281018733978271, 0.5725083351135254, 0.993952751159668]
 
-    
+    __timeout_strings = [
+        'reset pre',
+        'rst high 0',
+        'rst low 0',
+        'rst high 1',
+        'rst low 1',
+        'rst high 2',
+        'rst low 2',
+        'rst high 3',
+    ]
+
     @property
     def algorithm(self) -> int:
         return self.__algorithm
-    
+
     @algorithm.setter
     def algorithm(self, a: int):
         if a not in self.__ALGORITHMS:
@@ -80,11 +94,11 @@ class SimpleSerial2Dilithium(SimpleSerial2):
         # assert response is not None
         # assert response.startswith(b'set_alg ok: ' + struct.pack('B', a))
         self.__algorithm = a
-        
+
     @property
     def secret_key(self) -> bytes:
         return self.__secret_key
-    
+
     @property
     def crypto_secretkeybytes(self) -> int:
         return dilithium_params[self.__algorithm]['CRYPTO_SECRETKEYBYTES']
@@ -96,6 +110,10 @@ class SimpleSerial2Dilithium(SimpleSerial2):
     @property
     def polyz_packedbytes(self) -> int:
         return dilithium_params[self.__algorithm]['POLYZ_PACKEDBYTES']
+
+    @property
+    def dilithium(self):
+        return self.__d
     
     @secret_key.setter
     def secret_key(self, s: bytes):
@@ -103,6 +121,52 @@ class SimpleSerial2Dilithium(SimpleSerial2):
             raise ValueError(f'Expected secret key of length {self.crypto_bytes} but got {len(s)}')
         # self.send_cmd_long(self.__COMMAND_SECRET_KEY, s)
         self.__secre_key = s
+
+    __iteration_duration_cache = None
+
+    def dglitch_settings(self):
+        """
+        Dilithium glitch settings
+        """
+        self.scope.default_setup()
+        self.scope.cglitch_setup()  # default_setup for clock glitching
+        self.scope.clock.adc_src = "clkgen_x1"  # scope.adc.trig_count will be in units of clock cycles
+
+    def run_without_glitch(self, action: callable) -> None:
+        self.scope.io.hs2 = "clkgen"  # disable glitch
+        self.scope.sc.arm(False)  # reset trig_count
+        self.scope.arm()
+
+        action()
+
+        self.scope.io.hs2 = "glitch"  # enable glitch
+
+    @property
+    @functools.lru_cache(maxsize=None)  # replace with @functools.cache when switching to python3.9 or above
+    def loop_duration(self) -> int:
+        self.run_without_glitch(self.loop)
+        return self.scope.adc.trig_count
+
+    @property
+    def loop_duration_threshold(self):
+        # this constant is great enough that it filters almost all false positives but
+        # also always allows faults even in the last poly_index
+        return self.loop_duration - 30
+
+    @property
+    def iteration_duration(self) -> int:
+        """
+        This is an upper bound!
+        """
+        if self.__iteration_duration_cache is None:
+            self.scope.glitch_disable()
+            self.scope.arm()
+            self.loop()
+            timeout = self.scope.capture()
+            assert not timeout
+            self.__iteration_duration_cache = math.ceil(self.scope.adc.trig_count / self.__d._polyz_unpack_num_iters)
+            self.dglitch_settings()
+        return self.__iteration_duration_cache
 
     @property
     def all_possible_offsets(self) -> [int]:
@@ -141,13 +205,13 @@ class SimpleSerial2Dilithium(SimpleSerial2):
             step /= 10
         return widths
 
-    def offsets_which_include(self, lower_offset: float, upper_offset: float):
-        return self.sublist_which_include(lower_offset, upper_offset, self.all_possible_offsets)
+    def offsets_which_include(self, lower_offset: float, upper_offset: float, nonzero: bool = True):
+        return self.sublist_which_include(lower_offset, upper_offset, self.all_possible_offsets, nonzero=nonzero)
 
-    def widths_which_include(self, lower_width: float, upper_width: float):
-        return self.sublist_which_include(lower_width, upper_width, self.all_possible_widths)
+    def widths_which_include(self, lower_width: float, upper_width: float, nonzero: bool = True):
+        return self.sublist_which_include(lower_width, upper_width, self.all_possible_widths, nonzero=nonzero)
 
-    def sublist_which_include(self, lower_width: float, upper_width: float, data: set) -> [int]:
+    def sublist_which_include(self, lower_width: float, upper_width: float, data: set, nonzero: bool = True) -> [int]:
         lower_index = None
         upper_index = None
 
@@ -172,25 +236,128 @@ class SimpleSerial2Dilithium(SimpleSerial2):
             if lower_index is not None and upper_index is not None:
                 break
 
-        return self.all_possible_widths[lower_index: upper_index + 1]
+        ret = self.all_possible_widths[lower_index: upper_index + 1]
+        if nonzero:
+            ret = list(filter(lambda x: x != 0, ret))
+        return ret
 
     @property
     def scope(self):
         return self.__scope
-    
-    
+
+
     @scope.setter
     def scope(self, s):
         assert s is not None
         self.__scope = s
-        
+
     def sign(self, message: bytes, timeout: int = 10000) -> None:
+        self.sign_send(message)
+        return self.sign_recv(timeout)
+
+    def sign_recv(self, timeout: int = 10000):
         ok_reply = b'sign ok'
+        reply = self.simpleserial_read('r', len(ok_reply), timeout=timeout)
+        assert reply == ok_reply
+
+    def sign_send(self, message: bytes) -> None:
         if len(message) > self.__MAX_PAYLOAD_LENGTH:
             raise ValueError()
         self.send_cmd(self.__COMMAND_SIGN, 0, message)
-        reply = self.simpleserial_read('r', len(ok_reply), timeout=timeout)
-        assert reply == ok_reply
+
+    @property
+    def timeout_strings(self):
+        return tuple(self.__timeout_strings)
+
+    def timeout_index_to_str(self, index: int) -> str:
+        """
+        index returned from sign_no_rej
+        raises a value error if index is invalid
+        """
+        if index - 1 not in range(len(self.timeout_strings)):
+            raise ValueError('invalid timeout index')
+
+        return self.timeout_strings[index - 1]
+
+    def sign_no_rej(self, message: bytes, timeout: int = 10000) -> (float,):
+        from chipwhisperer.capture.scopes._OpenADCInterface import STATUS_EXT_MASK, ADDR_STATUS
+
+        # from a random measurement
+        TIME_PRE = 16718143905996707 - 16718143597047553
+        TIME_HIGH = 16718143906529010 - 16718143905996707
+        TIME_LOW = 16718143919473271 - 16718143906529010
+
+        TIMEOUT_PRE_NS = TIME_PRE + round(.8 * TIME_LOW)
+        TIMEOUT_HIGH_NS = TIME_HIGH * 5  # low is waaay lomger than high
+        TIMEOUT_LOW_NS = TIME_LOW + round(.8 * TIME_LOW)
+
+
+        handle = self.scope.adc.oa.serial.usbtx.handle  # directly work on libusb1 python package level
+        dat_status_out = bytearray([1, 0, 0, 0, 2, 0, 0, 0])
+
+        state_previous = False
+        assert not state_previous  # trigger should be low
+
+        gen_y_timeout_ns = 500e6  # half a second
+        i_break = 2 * self.__d.l + 1
+
+        i = 1
+        delta = 1  # if it is initialitzed is it faster?
+        timings = list(-1 for _ in range(self.__d.l * 2 + 1))
+        counts = list(0 for _ in range(self.__d.l * 2 + 1))
+        trig_counts = list(-1 for _ in range(self.__d.l * 2 + 1))
+
+        self.scope.sc.arm(False)
+        self.scope.arm()
+        assert not self.scope.adc.state, "state should be low in the beginning"
+        timings[0] = time.perf_counter_ns()
+        self.sign_send(message)  # from here on time critical
+        while True:
+            # state_new = self.scope.adc.oa.serial.cmdReadMem(ADDR_STATUS, 1)[0] & STATUS_EXT_MASK  # == self.scope.adc.state
+            handle.controlWrite(0x41, 18, 0, 0, dat_status_out, timeout=20000)
+            state_new = handle.controlRead(0xC1, 18, 0, 0, 1, timeout=20000)[0] & STATUS_EXT_MASK
+            # state_new = scope.adc.state
+            counts[i] += 1
+            if state_new != state_previous:  # edge
+                timings[i] = time.perf_counter_ns()
+                if not state_new:  # falling edge, now trig_count should be valid according to documentation
+                    # while low we have way more time compared to being high
+                    trig_counts[i] = self.scope.adc.trig_count  # inefficent, but sufficient?
+                    self.scope.sc.arm(False)  # resets trig_count
+                    self.scope.io.hs2 = "clkgen"  # disable glitch for the following poly samples
+                    self.scope.arm()  # activates trig_count
+                i += 1
+            else:  # check for timeout
+                delta = time.perf_counter_ns() - timings[i - 1]
+                if i == 1 and delta > TIMEOUT_PRE_NS:
+                    # could be that we missed high because of fault, better log last trig_count
+                    trig_counts[2] = self.scope.adc.trig_count
+                    break
+                if i != 1 and i % 2 == 0 and delta > TIMEOUT_HIGH_NS:  # even means high
+                    break
+                if i != 1 and i % 2 == 1 and delta > TIMEOUT_LOW_NS:  # low
+                    # could be that we missed high because of fault, better log last trig_count
+                    trig_counts[i] = self.scope.adc.trig_count
+                    break
+            if i == i_break:  # check if done
+                # not time critical anymore
+                break
+
+            state_previous = state_new
+        # not time critical anymore
+
+        # trim arrays
+        counts = counts[1:]
+        trig_counts = trig_counts[2::2]
+
+        self.scope.io.hs2 = "glitch"  # enable glitches again for the next run
+
+        self.__logger.warning(f'timings: {timings}')
+        self.__logger.warning(f'counts: {counts}')
+        self.__logger.warning(f'trig_counts: {trig_counts}')
+
+        self.sign_recv(timeout)
+        return timings, counts, trig_counts, None if i == i_break else self.timeout_index_to_str(i)
 
     def loop_send(self) -> None:
         self.send_cmd(self.__COMMAND_LOOP, 0, b'')
@@ -241,6 +408,11 @@ class SimpleSerial2Dilithium(SimpleSerial2):
         self.scope.io.nrst = False
         time.sleep(0.05)
         self.scope.io.nrst = "high_z"
+        return self.wait_for_boot_msg(timeout)
+
+    def wait_for_boot_msg(self, timeout, flush: bool = False):
+        if flush:
+            self.flush()
         # why is the overhead byte \x0b? Is it always that value? We will see ...
         data_read = self.read_until_blocking(b'\x0bb\x07boot ok\xc1\x00', timeout=timeout) # simpleserial_put('b', sizeof("boot ok") - 1, "boot ok");
         return data_read
@@ -249,17 +421,17 @@ class SimpleSerial2Dilithium(SimpleSerial2):
         assert n > 0
         expected = b'set_alg ok: 3' + 100 * b'Hello'
         expected = expected[:self.__MAX_PAYLOAD_LENGTH]
-        
+
         def get_index_of_first_diff_bytes(a: bytes, b: bytes) -> int:
             if a == b:
                 return None
-            
+
             minlen = min(len(a), len(b))
             assert minlen >= 1
             for i in range(1, minlen):
                 if a[:i] != b[:i]:
                     return i - 1
-                
+
         dist = np.zeros(len(expected))
         for i in range(n):
             self.send_cmd(self.__COMMAND_ALGORITHM, 0, struct.pack('B', 3))
@@ -280,7 +452,7 @@ class SimpleSerial2Dilithium(SimpleSerial2):
                 else:
                     dist[diff_idx] += 1;
         return dist
-    
+
     def simpleserial_read(self, cmd=None, pay_len=None, end='\n', timeout=250, ack=True):
         self.__handler.reset()
         ret = super(SimpleSerial2Dilithium, self).simpleserial_read(cmd=cmd, pay_len=pay_len, end=end, timeout=timeout, ack=ack)
@@ -318,19 +490,19 @@ class SimpleSerial2Dilithium(SimpleSerial2):
 
     def __init__(self, scope = None, algorithm: int = 2, secret_key: bytes = None):
         super().__init__()
-        
+
         self.__scope = None
         self.__algorithm = algorithm
         self.algorithm = algorithm
         self.__d = Dilithium(self.algorithm)
         # self.__crypto_bytes = dilithium_params[self.__algorithm]['CRYPTO_SERETKEYBTES']
-        
+
         if secret_key is None:
             self.__d.pseudorandombytes_seed(b'attack-shuffling-countermeasure-keypair')
             public_key, secret_key = self.__d.keypair()
-        self.__secret_key = None
-        self.secret_key = secret_key
-        
+        self.__secret_key = secret_key
+        # self.secret_key = secret_key
+
         if scope is not None:
             self.__scope.default_setup()
 
